52d0135e1c919060e316cef6cda40154c5ef2ecd
shourya-2407 <shourya2407@gmail.com>
2025-03-04 23:31:39 +0530

    Update 4th_March.java

--- FinFactor Interview/4th_March.java
+++ FinFactor Interview/4th_March.java
@@ -4,6 +4,65 @@ March 4th: DSA & Java Core
 Topics: Arrays & Strings, Linked List Questions:
 * Easy:
     * Find the second largest element in an array
+      
+   Brute Force:
+   class Solution {
+        public int getSecondLargest(int[] arr) {
+           // Code Here
+           int n = arr.length;
+           Set<Integer> set = new HashSet<>();
+           
+           for(int i = 0; i < n; i++){
+               set.add(arr[i]);
+           }
+           
+           List<Integer> list = new ArrayList<Integer>(set); 
+           Collections.sort(list, Collections.reverseOrder());
+           
+           if(list.size() < 2) return -1;
+           
+           return list.get(1);
+       }
+   }
+
+   Better Brute Force:
+   class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n = arr.length;
+        
+        Arrays.sort(arr);
+        int largestEle = arr[n - 1];
+        
+        for(int i = n - 2; i >= 0; i--){
+            if(arr[i] != largestEle) return arr[i];
+        }
+        
+        return -1;
+     }
+   }
+
+   Optimized using 2 pointers:
+   class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n = arr.length;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+        
+        return (secondLargest != Integer.MIN_VALUE) ? secondLargest : -1 ;
+     }
+   }
+   
     * Reverse a linked list (iterative & recursive)
 * Medium:
     * Two Sum (LeetCode #1)
