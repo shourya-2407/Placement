1ae58d711148e38add5747a94de2c19d8e1b427b
shourya-2407 <shourya2407@gmail.com>
2025-03-05 22:12:01 +0530

    Update 4th_March.java

--- FinFactor Interview/4th_March.java
+++ FinFactor Interview/4th_March.java
@@ -138,7 +138,59 @@ Optimisation techniques:
 
 * Medium:
     * Two Sum (LeetCode #1)
+
+   Brute Force:
+   class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+     }
+   }
+
+   Optimized Approach using HashMap: 
+   class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> _map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            if(_map.containsKey(nums[i]) && _map.get(nums[i]) != i){
+                return new int[]{i, _map.get(nums[i])};
+            }
+            _map.put(target - nums[i], i);
+        }
+
+        return new int[]{};
+    }
+   }
+
+   Fastest approach apparently using brute force:
+   class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int finish = n - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                } else if (nums[finish - i] + nums[finish - j] == target) {
+                    return new int[]{finish - i, finish - j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+   }
+               
     * Longest Substring Without Repeating Characters (LeetCode #3)
+      
+       
 * Hard (Optional):
     * Trapping Rain Water (LeetCode #42)
 
