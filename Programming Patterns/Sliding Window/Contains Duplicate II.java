e6014e96da1f668db299029a26f97b41ef8da244
shourya-2407 <shourya2407@gmail.com>
2025-03-07 09:08:48 +0530

    Update Contains Duplicate II.java

--- Programming Patterns/Sliding Window/Contains Duplicate II.java
+++ Programming Patterns/Sliding Window/Contains Duplicate II.java
@@ -58,12 +58,12 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for(int i = 0; i < nums.length; i++){
-            if(set.size() > k){
-                set.remove(nums[i - k - 1]);
-            }           
             if(!set.add(nums[i])){
                 return true;
             }
+            if(set.size() > k){
+                set.remove(nums[i - k]);
+            }           
         }
 
         return false;
